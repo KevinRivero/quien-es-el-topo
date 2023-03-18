56c270e1d2e1cbaf2091e61c5c80ac3fe6a57911
KevinRivero <kevin_xavier_rivero@hotmail.com>
2023-03-18 17:36:05 -0300

    se agrega pagina nueva y funcionalidades

--- main.js
+++ main.js
@@ -1,82 +1,86 @@
 // objeto literal con las personas y sus atributos
 
-const personas = [{
-    nombre : 'Kevin',
-    genero : "masculino",
-    edad : 26,
+const personas = [
+  {
+    nombre: "Kevin",
+    genero: "masculino",
+    edad: 26,
     trabajoIT: false,
-    tieneTrabajo : true,
+    tieneTrabajo: true,
     hablaEnClase: true,
-    moquero : true,
-    fama : true
-},
-{
-    nombre : 'Juane',
-    genero : "masculino",
-    edad : 22,
+    moquero: true,
+    fama: true,
+  },
+  {
+    nombre: "Juane",
+    genero: "masculino",
+    edad: 22,
     trabajoIT: false,
-    tieneTrabajo : false,
+    tieneTrabajo: false,
     hablaEnClase: false,
-    moquero : false,
-    fama: true
-},
-{
-    nombre : 'Angel',
-    genero : "masculino",
-    edad : 24,
+    moquero: false,
+    fama: true,
+  },
+  {
+    nombre: "Angel",
+    genero: "masculino",
+    edad: 24,
     trabajoIT: true,
-    tieneTrabajo : true,
+    tieneTrabajo: true,
     hablaEnClase: true,
-    moquero : false,
-    fama: false
-},
-{
-    nombre : 'Mariel C',
-    genero : "femenino",
-    edad : 30,
+    moquero: false,
+    fama: false,
+  },
+  {
+    nombre: "Mariel C",
+    genero: "femenino",
+    edad: 30,
     trabajoIT: false,
-    tieneTrabajo : true,
+    tieneTrabajo: true,
     hablaEnClase: true,
-    moquero : false,
-    fama : false
-},
-{
-    nombre : 'Anthony',
-    genero : "masculino",
-    edad : 15,
+    moquero: false,
+    fama: false,
+  },
+  {
+    nombre: "Anthony",
+    genero: "masculino",
+    edad: 15,
     trabajoIT: false,
-    tieneTrabajo : false,
+    tieneTrabajo: false,
     hablaEnClase: true,
-    moquero : true,
-    fama : false
-},
-{
-    nombre : 'Gi',
-    genero : "femenino",
-    edad : 32,
+    moquero: true,
+    fama: false,
+  },
+  {
+    nombre: "Gi",
+    genero: "femenino",
+    edad: 32,
     trabajoIT: false,
-    tieneTrabajo : true,
+    tieneTrabajo: true,
     hablaEnClase: true,
-    moquero : false,
-    fama: false
-},
-{
-    nombre : 'Mati N',
-    genero : "Masculino",
-    edad : 32,
+    moquero: false,
+    fama: false,
+  },
+  {
+    nombre: "Mati N",
+    genero: "masculino",
+    edad: 32,
     trabajoIT: true,
-    tieneTrabajo : true,
+    tieneTrabajo: true,
     hablaEnClase: true,
-    moquero : false,
-    fama: false
-}
-]
+    moquero: false,
+    fama: false,
+  },
+];
 
-
-// 
+// ---------------------Pagina de Inicio-------------------
 
 let btnJugar = document.getElementById('jugar')
-btnJugar.addEventListener('click', e =>{
-    alert('click');
-})
+if(btnJugar){
+    btnJugar.addEventListener('click', (e) =>{
+        window.location = './hombreOMujer.html'
+    })
+}
+
+// ------------------ Pagina hombre o mujer ---------------
 
