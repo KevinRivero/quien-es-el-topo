107900ee598458bdf3e42396d4b1cfb9fbb161cd
KevinRivero <kevin_xavier_rivero@hotmail.com>
2023-03-18 15:17:20 -0300

    se crea repositorio con pagina de inicio lista

---
+++ main.js
@@ -0,0 +1,76 @@
+// objeto literal con las personas y sus atributos
+
+const personas = [{
+    nombre : 'Kevin',
+    genero : "masculino",
+    edad : 26,
+    trabajoIT: false,
+    tieneTrabajo : true,
+    hablaEnClase: true,
+    moquero : true,
+    fama : true
+},
+{
+    nombre : 'Juane',
+    genero : "masculino",
+    edad : 22,
+    trabajoIT: false,
+    tieneTrabajo : false,
+    hablaEnClase: false,
+    moquero : false,
+    fama: true
+},
+{
+    nombre : 'Angel',
+    genero : "masculino",
+    edad : 24,
+    trabajoIT: true,
+    tieneTrabajo : true,
+    hablaEnClase: true,
+    moquero : false,
+    fama: false
+},
+{
+    nombre : 'Mariel C',
+    genero : "femenino",
+    edad : 30,
+    trabajoIT: false,
+    tieneTrabajo : true,
+    hablaEnClase: true,
+    moquero : false,
+    fama : false
+},
+{
+    nombre : 'Anthony',
+    genero : "masculino",
+    edad : 15,
+    trabajoIT: false,
+    tieneTrabajo : false,
+    hablaEnClase: true,
+    moquero : true,
+    fama : false
+},
+{
+    nombre : 'Gi',
+    genero : "femenino",
+    edad : 32,
+    trabajoIT: false,
+    tieneTrabajo : true,
+    hablaEnClase: true,
+    moquero : false,
+    fama: false
+},
+{
+    nombre : 'Mati N',
+    genero : "Masculino",
+    edad : 32,
+    trabajoIT: true,
+    tieneTrabajo : true,
+    hablaEnClase: true,
+    moquero : false,
+    fama: false
+}
+]
+
+
+
